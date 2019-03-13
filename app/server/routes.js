f297d7edf2ec7392c70451239ff2f5a9448c74de
charlesgiry <charles.giry.laterriere@gmail.com>
2019-03-13 03:17:52 +0100

    corrected Server module bug

--- app/server/routes.js
+++ app/server/routes.js
@@ -61,7 +61,6 @@ module.exports = function(app, DB, func, validator) {
 	});
 	
 	// -------------------------- Server Invite Token -------------------------- //
-	// Need a massive rework
 	app.post('/server/invite', [
 		validator.check('token').not().isEmpty().trim().escape(),
 		validator.check('servertoken').not().isEmpty().trim().escape()
@@ -73,6 +72,37 @@ module.exports = function(app, DB, func, validator) {
 		}
 		
 		let {token, servertoken} = req.body;
+		let user = func.get_token(token);
+		let server = func.get_token(servertoken);
+		if ((user != null) && (user.type == 'user')) {
+			if ((server != null) && (server.type == 'server')) {
+				let sql = "SELECT _id FROM ServerUser WHERE userId = ? AND serverId = ?";
+				let db = DB.open();
+				db.get(sql, [user._id, server._id], function(err, row) {
+					if (err) {
+						let obj = {success: false, errors:[err.message]};
+						res.status(500).json(obj);
+					}
+					else if (row == null) {
+						let obj = {success: false, errors: ['Unauthorized']};
+						res.status(401).json(obj);
+					}
+					else {
+						let invite = func.gen_token('invite', server._id);
+						let obj = {success: true, inviteToken: invite};
+						res.status(200).json(obj)
+					}
+				});
+			}
+			else {
+				let obj = {success: false, errors:['Server Token error.']};
+				res.status(401).json(obj);
+			}
+		}
+		else {
+			let obj = {success: false, errors:['Token error.']};
+			res.status(401).json(obj);
+		}
 	});
 
 
@@ -89,54 +119,58 @@ module.exports = function(app, DB, func, validator) {
 		}
 		
 		// Request is formed properly
-		let {token, invite} = req.body;
+		let {token, invitetoken} = req.body;
 		let user = func.get_token(token);
-		let server = func.get_token(invite);
-		if (((user != null) && (user.type == "user")) && ((server != null) && (server.type == "invite"))) {
-			// Check user isn't already in the server
-			let sql = 'SELECT * FROM ServerUser WHERE serverId = ? AND userId = ?';
-			let db = DB.open();
-			db.get(sql, [server._id, user._id], function(err, row) {
-				if (err) {
-					let obj = {success: false, errors:[err.message]};
-					res.status(500).json(obj);
-				}
+		let server = func.get_token(invitetoken);
+		if ((user != null) && (user.type == "user")) {
+			if ((server != null) && (server.type == "invite")) {
 				
-				// No result, add it to DB
-				else if (row == null) {
-					let sql = "INSERT INTO ServerUser (serverId, userId) VALUES (?, ?)";
-					let db = DB.open();
-					db.run(sql, [server._id, user._id], function(err) {
+				// Check user isn't already in the server
+				let sql = 'SELECT * FROM ServerUser WHERE serverId = ? AND userId = ?';
+				let db = DB.open();
+				db.get(sql, [server._id, user._id], function(err, row) {
 					if (err) {
-						let obj = {success: false, errors: [err.message]};
+						let obj = {success: false, errors:[err.message]};
 						res.status(500).json(obj);
 					}
-					else {
-						let obj = {success: true};
-						res.status(200).json(obj);
+					
+					// No result, add it to DB
+					else if (row == null) {
+						let sql = "INSERT INTO ServerUser (serverId, userId) VALUES (?, ?)";
+						let db = DB.open();
+						db.run(sql, [server._id, user._id], function(err) {
+						if (err) {
+							let obj = {success: false, errors: [err.message]};
+							res.status(500).json(obj);
+						}
+						else {
+							let obj = {success: true};
+							res.status(200).json(obj);
+						}
+						});
 					}
-					});
-				}
-				// Result, Already in server user list
-				else {
-					let obj = {success: false, errors:['User is already in server\'s userlist.']};
-					res.status(401).json(obj);
-				}
-			});
-			DB.close();
+					// Result, Already in server user list
+					else {
+						let obj = {success: false, errors:['User is already in server userlist.']};
+						res.status(401).json(obj);
+					}
+				});
+				DB.close();
+			}
+			else {
+				let obj = {success: false, errors: ['Invite Token error.']};
+				res.status(500).json(obj);
+			}
 		}
 		else {
-			let errors = []
-			if (user == null || user.type != "user") {errors.push('Token error');}
-			if (server == null || server.type != "invite") {errors.push('Invite Token error');}
-			let obj = {success: false, errors: errors};
+			let obj = {success: false, errors: ['Token error.']};
 			res.status(500).json(obj);
 		}
 	});
 	
 	// -------------------------- Server list -------------------------- //
 	// returns a list of server you're a member of as tokens
-	app.post('/server/get', [
+	app.post('/server/in', [
 		validator.check('token').not().isEmpty().trim().escape()
 	], (req, res) => {
 		const errors = validator.validationResult(req);
@@ -169,6 +203,62 @@ module.exports = function(app, DB, func, validator) {
 				}
 			});
 			DB.close();
+		}
+		else {
+			let obj = {success: false, errors:['Token error.']};
+			res.status(401).json(obj);
+		}
+	});
+	
+	// -------------------------- User list -------------------------- //
+	app.post('/server/users', [
+		validator.check('token').not().isEmpty().trim().escape(),
+		validator.check('servertoken').not().isEmpty().trim().escape()
+	], (req, res) => {
+		const errors = validator.validationResult(req);
+		if (!errors.isEmpty()) {
+			let obj = {success: false, errors: errors.array()};
+			return res.status(422).json(obj);
+		}
+		
+		let {token, servertoken} = req.body;
+		let user = func.get_token(token);
+		let server = func.get_token(servertoken);
+		if ((user != null) && (user.type == 'user')) {
+			if ((server != null) && (server.type == 'server')) {
+				let sql = 'SELECT ServerUser.userId as uid, User.username as username FROM ServerUser JOIN User ON ServerUser.userId = User._id WHERE ServerUser.serverId = ?';
+				let db = DB.open();
+				db.all(sql, [server._id], function(err, rows) {
+					let result = false;
+					if (err) {
+						let obj = {success: false, errors:[err.message]};
+						res.status(500).json(obj);
+					}
+					else if (rows == null) {
+						let obj = {success: false, errors: ["Server doesn't exist"]};
+						res.status(500).json(obj);
+					}
+					else {
+						let users = [];
+						rows.forEach(function(row) {
+							users.push(row.username);
+							if (row.uid == user._id) {result = true;}
+						});
+						if (result) {
+							let obj = {success: true, users: users};
+							res.status(200).json(obj);
+						}
+						else {
+							let obj = {success: false, errors: ['Unauthorized']};
+						}
+					}
+				});
+				DB.close();
+			}
+			else {
+				let obj = {success: false, errors:['Server Token error.']};
+				res.status(401).json(obj);
+			}
 		}
 		else {
 			let obj = {success: false, errors:['Token error.']};
